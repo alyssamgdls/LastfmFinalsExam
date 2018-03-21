78e3d6288c9d3ee0020a7f670dc06c834eb424a7
Alyssa Marie Magdales <magdalesalyssa@yahoo.com>
2018-03-21 16:10:31 +0800

    Final

--- app/src/main/java/com/magdales/lastfmfinalsexam/AlbumAdapter.kt
+++ app/src/main/java/com/magdales/lastfmfinalsexam/AlbumAdapter.kt
@@ -1,18 +1,21 @@
 package com.magdales.lastfmfinalsexam
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_layout.view.*
 
 /**
  * Created by Lai on 3/21/2018.
  */
-class AlbumAdapter(val albumlist: ArrayList<AlbumDetails>) : RecyclerView.Adapter<AlbumAdapter.ViewHolder>() {
+class AlbumAdapter(var context: Context, val albumlist: ArrayList<Album>) : RecyclerView.Adapter<AlbumAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(context).inflate(R.layout.item_layout, parent, false)
         return ViewHolder(v)
     }
 
@@ -20,9 +23,18 @@ class AlbumAdapter(val albumlist: ArrayList<AlbumDetails>) : RecyclerView.Adapte
         return albumlist.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.nameText.text = albumlist[position].name
-        holder.artistText.text = albumlist[position].artist
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.itemView?.textView_name?.text = albumlist[position].name
+        holder?.itemView?.textView_artist?.text = albumlist[position].artist
+        val imgHolder = holder!!.itemView!!.imageView
+
+        if (albumlist[position].text == "null") {
+            Picasso.with(holder.itemView.context).load(R.drawable.ic_launcher_background).into(imgHolder)
+        } else {
+            Picasso.with(holder.itemView.context).load(albumlist[position].text).into(imgHolder)
+        }
+
+        Picasso.with(holder.itemView?.context).load(albumlist[position].text).into(imgHolder)
     }
 
 
--- app/src/main/java/com/magdales/lastfmfinalsexam/MainActivity.kt
+++ app/src/main/java/com/magdales/lastfmfinalsexam/MainActivity.kt
@@ -7,93 +7,68 @@ import android.support.v7.widget.RecyclerView
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.EditText
 import android.widget.LinearLayout
-import android.widget.Toast
-import com.google.gson.GsonBuilder
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.item_layout.*
 import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
 import android.widget.TextView
-import android.widget.TextView.OnEditorActionListener
-
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
 
     private var searchEntry: String? = null
+    private val albumlist = ArrayList<Album>()
 
-    private val API_KEY = "5bcca0696b69e6c62977cff0ce97372f"
-    private val API_SECRET = "b794a2f582e36e74835e3a4e9d7d302d"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rv = findViewById<RecyclerView>(R.id.recyclerView)
-        rv.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         editText_search?.setOnEditorActionListener(object : TextView.OnEditorActionListener {
             override fun onEditorAction(v: TextView, actionId: Int, event: KeyEvent?): Boolean {
                 var handled = false
+
+                searchEntry = editText_search.text.toString()
+                val url = "http://ws.audioscrobbler.com/2.0/?method=album.search&album=" + searchEntry + "&api_key=5bcca0696b69e6c62977cff0ce97372f&format=json"
+
+                progressBar.visibility = View.VISIBLE
+                lbl_no_album_present.visibility = View.GONE
+
                 if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    for (i in 0..49) {
+                        doAsync {
+                            val resultJson = URL(url).readText()
+                            val jsonObject = JSONObject(resultJson)
 
-                    searchEntry = editText_search.text.toString()
-                    progressBar.visibility = View.VISIBLE
-                    lbl_no_album_present.visibility = View.INVISIBLE
+                            val name = jsonObject.getJSONObject("results").getJSONObject("albummatches").getJSONArray("album").getJSONObject(i).getString("name")
+                            val artist = jsonObject.getJSONObject("results").getJSONObject("albummatches").getJSONArray("album").getJSONObject(i).getString("artist")
+                            var image = if (jsonObject.getJSONObject("results").getJSONObject("albummatches").getJSONArray("album").getJSONObject(i).getJSONArray("image").getJSONObject(2).getString("#text") == "") {
+                                jsonObject.getJSONObject("results").getJSONObject("albummatches").getJSONArray("album").getJSONObject(i).getJSONArray("image").getJSONObject(2).getString("")
+                            } else {
+                                jsonObject.getJSONObject("results").getJSONObject("albummatches").getJSONArray("album").getJSONObject(i).getJSONArray("image").getJSONObject(2).getString("#text")
+                            }
 
-                    val url = "http://ws.audioscrobbler.com/2.0/?method=album.search&album=" + searchEntry + "&api_key=" + API_KEY + "&format=json"
-                    val request = okhttp3.Request.Builder().url(url).build()
-                    val client = OkHttpClient.Builder().build()
+                            if (image == "") {
+                                image = "null"
+                            }
 
-                    client.newCall(request).enqueue(object : Callback {
-                        override fun onFailure(call: Call?, e: IOException?) {
-//                            Toast.makeText(this@MainActivity, "Request failed", Toast.LENGTH_SHORT).show()
-//                            progressBar.visibility = View.GONE
-                        }
-
-                        override fun onResponse(call: Call?, response: Response?) {
-                            if (response != null && response.isSuccessful) {
-                                val jsonObj = response.body()?.string()
-                                getData(jsonObj)
+                            uiThread {
+                                albumlist.add(Album(name, artist, image))
+                                recyclerView.adapter = AlbumAdapter(this@MainActivity, albumlist)
                             }
                         }
-                    })
+                    }
                     handled = true
                 }
                 return handled
             }
         })
-    }
-
-    private fun getData(jsonObj: String?){
-        runOnUiThread {
-            progressBar.visibility = View.GONE
-            val gsonObj = GsonBuilder().create()
-            val album = gsonObj.fromJson(jsonObj, AlbumDetails::class.java)
-
-            if (searchEntry == album.name) {
-                textView_name.text = album.name
-                textView_artist.text = album.artist
-                Picasso.with(this@MainActivity).load(album.image).into(imageView)
-                progressBar.visibility = View.GONE
-            }
-
-            val length = JSONObject(jsonObj).getJSONArray("results").length()
-            val albumlist = ArrayList<AlbumDetails>()
-            var pos = 0
-            for (i in 1..length) {
-                val albumName = JSONObject(jsonObj).getJSONArray("album").getJSONObject(pos).getString("name").toString()
-                val artistName = JSONObject(jsonObj).getJSONArray("album").getJSONObject(pos).getString("artist").toString()
-                val albumImage = JSONObject(jsonObj).getJSONArray("album").getJSONObject(pos).getJSONObject("image").getString("#text").toString()
-                albumlist.add(AlbumDetails(albumName, artistName, albumImage))
-                val adapter = AlbumAdapter(albumlist)
-                recyclerView.adapter = adapter
-                pos++
-            }
-        }
     }
 }
